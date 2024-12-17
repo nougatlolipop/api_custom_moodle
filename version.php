1c6e9b35ebea8c80080d6c590e732823e85522b0
nougatlolipop <august.ivan20@gmail.com>
2024-12-17 14:40:39 +0700

    first commit

--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_customquestionapi'; // Nama plugin (harus sama dengan folder).
-$plugin->version = 2024081900; // Versi plugin: TahunBulanTanggalXX.
+$plugin->version = 2024121700; // Versi plugin: TahunBulanTanggalXX.
 $plugin->requires = 2023042400; // Moodle 4.2.
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = 'v1.0';
